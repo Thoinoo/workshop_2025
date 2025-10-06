d931ae6e4f71f9ff06e593c6b552bcf8fe24acb5
antoine <thoino@outlook.fr>
2025-10-06 15:37:21 +0200

    fix majuscules

--- workshop-2025/src/App.jsx
+++ workshop-2025/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Accueil from "./pages/Accueil";
 import Jeu from "./pages/Jeu";
-import Enigme1 from "./pages/enigme1";
+import Enigme1 from "./pages/Enigme1";
 
 function App() {
   return (
