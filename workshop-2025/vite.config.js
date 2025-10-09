ace0e74c9c9069c42a17d71f56e38eea36b9cef9
antoine <thoino@outlook.fr>
2025-10-09 13:08:38 +0200

    maj url

--- workshop-2025/vite.config.js
+++ workshop-2025/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ["crypto-breakdown.ddns.net"],
     host: true,            // ← permet l'accès depuis ton réseau local (192.168.x.x)
     port: 5173,            // ← optionnel, fixe le port (par défaut 5173)
     proxy: {
